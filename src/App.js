7efc1ee8839bc00fa629e132a3f021e4ccb54fce
Batuhan <batuhanyigitsengul@gmail.com>
2022-09-28 15:05:52 +0300

    svg blurred circle cursor

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 function App() {
-const [mousePos, setMousePos] = useState({
-  x:0,
-  y:0
-})
-console.log(mousePos)
+  const [mousePos, setMousePos] = useState({
+    x: 0,
+    y: 0,
+  });
 
-useEffect(() => {
-  const mouseMove = e => {
-    setMousePos({
-      x: e.clientX,
-      y: e.clientY
-    })
-  }
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePos({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
 
-  window.addEventListener('mousemove', mouseMove)
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
 
-  return () => {
-    window.removeEventListener('mousemove', mouseMove)
-  }
-}, [])
-
-
-const variants = {
-  default: {
-    x: mousePos.x -16, //to center it (minus(cursorsize/2))
-    y: mousePos.y -16
-  }
-}
+  const variants = {
+    default: {
+      x: mousePos.x - 793, //to center it (minus(cursorsize/2))
+      y: mousePos.y - 374,
+    },
+  };
 
   return (
     <div className="noise-background">
-      <div className="text"><h1>CUSTOM CURSOR</h1></div>
-      <motion.div 
-      className="cursor"
-      variants={variants}
-      animate='default'
-      />
+      <div className="text">
+        <h1>CUSTOM CURSOR</h1>
+      </div>
+      <motion.div className="cursor" variants={variants} animate="default" />
     </div>
   );
 }
